aba55d8d3944e883129613271c551a232cb2f52b
psychobolt <psychobolt@gmail.com>
2018-04-11 21:17:26 -0700

    reorganize app code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import createHistory from 'history/createBrowserHistory';
 
 import './index.css';
 import App from './App';
-import reducers from './reducers';
+import reducer from './reducer';
 import registerServiceWorker from './registerServiceWorker';
 
 const history = createHistory();
 const composeEnhancers = composeWithDevTools({});
 const middleware = routerMiddleware(history);
 const store = createStore(
-  combineReducers({
-    ...reducers,
-    router: routerReducer,
-  }),
+  reducer,
   composeEnhancers(applyMiddleware(middleware)),
 );
 

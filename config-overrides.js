12ba89914e218bf9b2b672293584e2e29e75b67e
psychobolt <psychobolt@gmail.com>
2018-04-27 13:51:50 -0700

    Add stylelint

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,14 @@
 const rewirePolyfills = require('react-app-rewire-polyfills');
-const rewireLodash = require('react-app-rewire-lodash')
+const rewireLodash = require('react-app-rewire-lodash');
 const rewireStyledComponents = require('react-app-rewire-styled-components');
+const rewireStylelintCustomProcessorLoader = require('react-app-rewire-stylelint-custom-processor-loader');
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 
 module.exports = function override(config, env) {
   config = rewirePolyfills(config, env);
   config = rewireLodash(config, env, { id: ['lodash', 'recompose'] });
   config = rewireStyledComponents(config, env);
+  config = rewireStylelintCustomProcessorLoader(config, env);
   config = rewireReactHotLoader(config, env);
   return config;
-};
+}

3ba4b79475071f307a08386baf31ef8e41222754
psychobolt <psychobolt@gmail.com>
2018-05-02 15:58:39 -0700

    Improve test coverage

--- src/App/__tests__/App.component-test.js
+++ src/App/__tests__/App.component-test.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import App from '../App.component';
+import { FilterRoutes } from '../TodoList';
 
 describe('component <App /> renders without crashing', () => {
   it('renders with location state', () => {
     shallow(<App location={{ state: {} }} />);
+  });
+
+  it('renders with location pathname', () => {
+    shallow(<App location={{ pathname: FilterRoutes.ACTIVE.pathname }} />);
   });
 
   it('renders without location state', () => {
